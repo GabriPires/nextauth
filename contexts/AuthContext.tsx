64a9ac034f9f9bb0e4e917ed9eed45b71688a551
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-30 19:56:18 -0300

    feat: using nookies to save cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { createContext, useState } from 'react';
+import { setCookie } from 'nookies';
 import { api } from '../services/api';
 
 type User = {
@@ -39,6 +40,16 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       });
 
       const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
 
       setUser({
         email: email,
